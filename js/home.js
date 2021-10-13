837edca5ed8a743b3e9f81baeac5cfff028f5cd3
Paulo Sergio Duarte Lima <paulo.lima@suppliercard.com.br>
2021-10-13 12:53:37 -0300

    testes

--- js/home.js
+++ js/home.js
@@ -1,3 +1,9 @@
+//habilitar whatsapp
+var enableButtonWhatsapp = "0";
+if($('#enableButtonWhatsapp').val()){
+    enableButtonWhatsapp = $('#enableButtonWhatsapp').val();
+}
+
 //mudar cor do menu ao rolar scroll
 $(window).scroll(function () {
     if ($(this).scrollTop()) {
@@ -9,14 +15,16 @@ $(window).scroll(function () {
 });
 
 //Whatsapp button
-(function () {
-    var options = {
-        whatsapp: "+55(11)98717-5315", // WhatsApp number
-        call_to_action: "Fale Conosco Agora!", // Call to action
-        position: "left", // Position may be 'right' or 'left'
-    };
-    var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
-    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-})();
+if(enableButtonWhatsapp == "1"){
+    (function () {
+        var options = {
+            whatsapp: "+55(11)98717-5315", // WhatsApp number
+            call_to_action: "Fale Conosco Agora!", // Call to action
+            position: "left", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+}
