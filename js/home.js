03898f5cb82a9be6415fd98d459d9b98bf61fc4f
Paulo Sergio Duarte Lima <paulo.lima@suppliercard.com.br>
2021-10-13 13:19:00 -0300

    ajustes menu

--- js/home.js
+++ js/home.js
@@ -8,9 +8,11 @@ if($('#enableButtonWhatsapp').val()){
 $(window).scroll(function () {
     if ($(this).scrollTop()) {
         $('.navbar-custom').attr('style',"background-color: rgba(1, 17, 112, 0.5)!important");
+        $('.dropdown-menu').attr('style',"background-color: rgba(1, 17, 112, 0.5)!important");
     }else{
         
         $('.navbar-custom').attr('style',"background-color: #011170!important");
+        $('.dropdown-menu').attr('style',"background-color: #011170!important");
     }
 });
 
