c109c6aff25114ee040a1861018a20966aada49c
Paulo <paulo_sergio_duarte@hotmail.com>
2022-01-29 11:30:14 -0300

    2022

--- js/main.js
+++ js/main.js
@@ -42,7 +42,7 @@ var htmlFooter = `
 <div class="container-fluid">
 <div class="row">
  <div class="col-md-3 col-xs-10 offset-xs-2 contact-box text-center">
-  <p class="main-tittle">Copyright &copy; 2021 The Thester</p>        
+  <p class="main-tittle">Copyright &copy; ` + new Date().getFullYear + ` The Thester</p>        
   <div style="height: 20px;"></div>
  </div>
  
